87fc8b67f4bda6f3fefa495a75c8088d0c69e229
futureniyi <futureniyi@yahoo.com>
2025-11-10 23:01:10 +0100

    assignment 2

---
+++ database/assignment2.sql
@@ -0,0 +1,45 @@
+-- Data for table `account`
+INSERT INTO public.account (
+        account_firstname,
+        account_lastname,
+        account_email,
+        account_password
+    )
+VALUES (
+        'Tony',
+        'Stark',
+        'tony@starkent.com',
+        'Iam1ronM@n'
+    );
+-- Modify the Tony Stark Record
+UPDATE public.account
+SET account_type = 'Admin'
+WHERE account_id = 1;
+-- Delete the Tony Stark record from the database.
+DELETE FROM public.account
+WHERE account_id = 1;
+-- Modify the "GM Hummer" record to read "a huge interior"
+UPDATE public.inventory
+SET inv_description = REPLACE(
+        inv_description,
+        'small interiors',
+        'a huge interior'
+    )
+WHERE inv_id = 10;
+-- Use an inner join to select the make and 
+-- model fields from the inventory table and 
+-- the classification name field from the 
+-- classification table for inventory items 
+-- that belong to the "Sport" category. 
+SELECT i.inv_make,
+    i.inv_model,
+    c.classification_name
+FROM inventory AS i
+    JOIN classification AS c ON c.classification_id = i.classification_id
+WHERE c.classification_name = 'Sport';
+-- Update all records in the inventory table to add "/vehicles" 
+--to the middle of the file path in the inv_image and inv_thumbnail 
+--columns using a single query. 
+UPDATE public.inventory
+SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+    inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -234,3 +234,28 @@ VALUES (
         'White',
         5
     );
+-- Modify the "GM Hummer" record to read "a huge interior"
+UPDATE public.inventory
+SET inv_description = REPLACE(
+        inv_description,
+        'small interiors',
+        'a huge interior'
+    )
+WHERE inv_id = 10;
+-- Use an inner join to select the make and 
+-- model fields from the inventory table and 
+-- the classification name field from the 
+-- classification table for inventory items 
+-- that belong to the "Sport" category. 
+SELECT i.inv_make,
+    i.inv_model,
+    c.classification_name
+FROM inventory AS i
+    JOIN classification AS c ON c.classification_id = i.classification_id
+WHERE c.classification_name = 'Sport';
+-- Update all records in the inventory table to add "/vehicles" 
+--to the middle of the file path in the inv_image and inv_thumbnail 
+--columns using a single query. 
+UPDATE public.inventory
+SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+    inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
