e762ca1a60d462eef95b876e4c35d1f7d44ba658
futureniyi <futureniyi@yahoo.com>
2025-11-22 18:53:46 +0100

    task 3

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -6,4 +6,13 @@ baseController.buildHome = async function(req, res){
   res.render("index", {title: "Home", nav})
 }
 
+/* ***************************
+ *  Intentional 500 error trigger
+ * ************************** */
+baseController.triggerError = async function(req, res){
+  const err = new Error("Intentional server error for testing.")
+  err.status = 500
+  throw err
+}
+
 module.exports = baseController
--- routes/static.js
+++ routes/static.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const utilities = require("../utilities/")
+const baseController = require("../controllers/baseController")
 
 // Static Routes
 // Set up "public" folder / subfolders for static files
+router.get("/trigger-error", utilities.handleErrors(baseController.triggerError))
 router.use(express.static("public"));
 router.use("/css", express.static(__dirname + "public/css"));
 router.use("/js", express.static(__dirname + "public/js"));
 router.use("/images", express.static(__dirname + "public/images"));
 
 module.exports = router;
-
 
 
