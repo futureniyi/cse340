c5c1f934cd8878992ad59e3872028300db2c9d04
futureniyi <futureniyi@yahoo.com>
2025-11-29 21:04:32 +0100

    Password Hashing

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -7,7 +7,12 @@ const regValidate = require('../utilities/account-validation')
 
 // Route to build "My Account" login view (path after "account")
 router.get("/login", utilities.handleErrors(accountsController.buildLogin))
-// router.post("/login", utilities.handleErrors(accountsController.loginAccount))
+router.post(
+  "/login",
+  regValidate.loginRules(),
+  regValidate.checkLoginData,
+  utilities.handleErrors(accountsController.loginAccount)
+)
 
 // Route to build registration view
 router.get("/register", utilities.handleErrors(accountsController.buildRegister))
